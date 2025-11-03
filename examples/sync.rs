cb6f096eac6c7465f32b3d210c966aa233148388
Marius Pardo <pardomarius@gmail.com>
2025-11-03 12:54:40 +0100

    Update sync example to showcase bi directionnal communication

--- examples/sync.rs
+++ examples/sync.rs
@@ -11,9 +11,20 @@ trait CounterManager {
     fn get() -> i32;
 }
 
-fn manager_thread(rx: Receiver<CounterManagerMessage>) {
+#[channel_protocol]
+trait CounterOutgoing {
+    fn reached_10();
+    fn multiple_of_5(val: i32);
+}
+
+fn manager_thread(
+    counter_outgoing_client: CounterOutgoingClient,
+    rx: Receiver<CounterManagerMessage>,
+) {
+    let mut prev_counter = 0;
     let mut counter = 0;
     for message in rx {
+        prev_counter = counter;
         match message {
             CounterManagerMessage::Inc { i } => {
                 counter += i;
@@ -32,22 +43,50 @@ fn manager_thread(rx: Receiver<CounterManagerMessage>) {
                 counter += i;
             }
         }
+        if prev_counter != counter {
+            if counter == 10 {
+                counter_outgoing_client.reached_10();
+            }
+            if counter % 5 == 0 {
+                counter_outgoing_client.multiple_of_5(counter);
+            }
+        }
     }
 }
 
 fn main() {
     let (counter_client, counter_manager_rx) = CounterManagerClient::new();
+    let (counter_outgoing_client, counter_outgoing_rx) = CounterOutgoingClient::new();
     thread::spawn(|| {
-        manager_thread(counter_manager_rx);
+        manager_thread(counter_outgoing_client, counter_manager_rx);
     });
 
-    assert_eq!(0, counter_client.get());
+    thread::spawn(|| {
+        for message in counter_outgoing_rx {
+            match message {
+                CounterOutgoingMessage::Reached10 => {
+                    println!("Counter reached 10!");
+                }
+                CounterOutgoingMessage::MultipleOf5 { val } => {
+                    println!("Counter is multiple of 5: {val}");
+                }
+            }
+        }
+    });
+
+    assert_eq!(0, counter_client.get()); // This should trigger the "multiple of 5" message
     counter_client.inc(2);
     assert_eq!(2, counter_client.get());
     counter_client.dec(1);
     assert_eq!(1, counter_client.get());
     counter_client.reset();
     assert_eq!(0, counter_client.get());
-    assert_eq!(0, counter_client.get_and_inc(5));
+    assert_eq!(0, counter_client.get_and_inc(5)); // This should trigger the "multiple of 5" message
     assert_eq!(5, counter_client.get());
+    counter_client.inc(4);
+    assert_eq!(9, counter_client.get());
+    counter_client.inc(1); // This should trigger the "reached 10" and "multiple of 5" message
+    assert_eq!(10, counter_client.get());
+    counter_client.inc(5); // This should trigger the "multiple of 5" message
+    assert_eq!(15, counter_client.get());
 }
